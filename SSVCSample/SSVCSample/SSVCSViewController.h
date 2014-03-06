3283a094eec5e546d664c612886c2dcfdc5fd4f8
Tom Elliott <tomelliott@fb.com>
2014-03-06 09:25:25 +0000

    Sample app

--- SSVCSample/SSVCSample/SSVCSViewController.h
+++ SSVCSample/SSVCSample/SSVCSViewController.h
@@ -10,4 +10,18 @@
 
 @interface SSVCSViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UILabel *feedbackLabel;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *feedbackIndicator;
+
+@property (strong, nonatomic) IBOutlet UILabel *updateAvailableTitle;
+@property (strong, nonatomic) IBOutlet UILabel *updateAvailableResult;
+@property (strong, nonatomic) IBOutlet UILabel *updateRequiredTitle;
+@property (strong, nonatomic) IBOutlet UILabel *updateRequiredResult;
+@property (strong, nonatomic) IBOutlet UILabel *updateSinceTitle;
+@property (strong, nonatomic) IBOutlet UILabel *updateSinceResult;
+@property (strong, nonatomic) IBOutlet UILabel *latestVersionKeyTitle;
+@property (strong, nonatomic) IBOutlet UILabel *latestVersionKeyResult;
+@property (strong, nonatomic) IBOutlet UILabel *latestVersionNumberTitle;
+@property (strong, nonatomic) IBOutlet UILabel *latestVersionNumberResult;
+
 @end
--- SSVCSample/SSVCSample/SSVCSViewController.m
+++ SSVCSample/SSVCSample/SSVCSViewController.m
@@ -8,22 +8,67 @@
 
 #import "SSVCSViewController.h"
 
+#import <SSVC/SSVC.h>
+#import <SSVC/SSVCResponse.h>
+
 @interface SSVCSViewController ()
+
+@property (nonatomic, strong) SSVC *versionChecker;
 
 @end
 
 @implementation SSVCSViewController
 
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+  if (self = [super initWithCoder:aDecoder]) {
+    __weak SSVCSViewController *weakSelf = self;
+    
+    // Create the version checker and the callback blocks
+    _versionChecker = [[SSVC alloc] initWithCompletionHandler:^(SSVCResponse *response) {
+      SSVCSViewController *strongSelf = weakSelf;
+      if (strongSelf) {
+        strongSelf.feedbackIndicator.hidden = YES;
+        strongSelf.feedbackLabel.text = @"Version Check Success!";
+        
+        strongSelf.updateAvailableResult.text = response.updateAvailable ? @"Yes" : @"No";
+        strongSelf.updateRequiredResult.text = response.updateRequired ? @"Yes" : @"No";
+        
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setTimeStyle:NSDateFormatterNoStyle];
+        [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+        strongSelf.updateSinceResult.text = [dateFormatter stringFromDate:response.updateAvailableSince];
+        
+        strongSelf.latestVersionKeyResult.text = response.versionKey;
+        strongSelf.latestVersionNumberResult.text = [NSString stringWithFormat:@"%@", response.versionNumber];
+      }
+    } failureHandler:^(NSError *error) {
+      SSVCSViewController *strongSelf = weakSelf;
+      if (strongSelf) {
+        strongSelf.feedbackIndicator.hidden = YES;
+        strongSelf.feedbackLabel.text = @"Update Failed";
+        NSLog(@"%@", [error localizedDescription]);
+      }
+    }];
+  }
+  return self;
+}
+
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+
+  // Check version
+  [_feedbackIndicator startAnimating];
+  _feedbackIndicator.hidden = NO;
+  _feedbackLabel.text = @"Checking for Update";
+  [_versionChecker checkVersion];
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 @end
