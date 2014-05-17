ff4a46da05c77728bad427edb6846a79fb92f999
Tom Elliott <tomelliott@fb.com>
2014-05-18 00:44:12 +0100

    Update and impreove url generator tests

--- SSVCTests/SSVCURLGeneratorTests.m
+++ SSVCTests/SSVCURLGeneratorTests.m
@@ -43,9 +43,12 @@
   NSString *urlString = [url absoluteString];
   
   NSRange range = [urlString rangeOfString:@"?" options:NSBackwardsSearch];
+  NSUInteger numberOfOccurancesOfQM = [self numberOfOccurancesOfNeedle:@"?" inHaystack:urlString];
+  NSUInteger numberOfOccurancesOfA = [self numberOfOccurancesOfNeedle:@"&" inHaystack:urlString];
   
   XCTAssertFalse(range.location == NSNotFound, @"A ? must exist in the URL");
-  XCTAssertEqual(range.length, 1u, @"Only one ? character should be found together");
+  XCTAssertEqual(@1, @(numberOfOccurancesOfQM), @"Only one ? character should be found together");
+  XCTAssertEqual(@4, @(numberOfOccurancesOfA), @"4 & characters should be found all together");
 }
 
 - (void)testURLGeneratorAppendsAmpersandForSeperatorWhenBaseURLAlreadyContainsGETParams
@@ -62,12 +65,57 @@
   NSString *urlString = [url absoluteString];
   
   NSRange range = [urlString rangeOfString:@"&" options:NSBackwardsSearch];
+  NSUInteger numberOfOccurancesOfQM = [self numberOfOccurancesOfNeedle:@"?" inHaystack:urlString];
+  NSUInteger numberOfOccurancesOfA = [self numberOfOccurancesOfNeedle:@"&" inHaystack:urlString];
   
   XCTAssertFalse(range.location == NSNotFound, @"A & must exist in the URL");
-  XCTAssertEqual(range.length, 1u, @"Only one & character should be found together");
+  XCTAssertEqual(@1, @(numberOfOccurancesOfQM), @"Only one ? character should be found together");
+  XCTAssertEqual(@5, @(numberOfOccurancesOfA), @"5 & characters should be found all together");
 }
 
 - (void)testURLGeneratorContainsVersionValueForVersionKey
+{
+  NSString *baseURL = @"http://www.foo.comr";
+  NSString *versionKey = @"version_key";
+  NSNumber *versionNumber = @1;
+  
+  SSVCURLGenerator *urlGenerator = [[SSVCURLGenerator alloc] initWithBaseURL:baseURL
+                                                                  versionKey:versionKey
+                                                               versionNumber:versionNumber];
+  
+  NSURL *url = [urlGenerator url];
+  NSString *urlString = [url absoluteString];
+  
+  NSString *getKV = [NSString stringWithFormat:@"%@=%@", SSVCLatestVersionKey, versionKey];
+  NSRange range = [urlString rangeOfString:getKV options:0];
+  NSUInteger numberOfOccurances = [self numberOfOccurancesOfNeedle:getKV inHaystack:urlString];
+  
+  XCTAssertFalse(range.location == NSNotFound, @"Key-value pair must occur in GET params");
+  XCTAssertEqual(@1, @(numberOfOccurances), @"Only one kv pair should be found together");
+}
+
+- (void)testURLGeneratorContainsVersionNumberValueForVersionNumberKey
+{
+  NSString *baseURL = @"http://www.foo.com";
+  NSString *versionKey = @"version_key";
+  NSNumber *versionNumber = @1;
+  
+  SSVCURLGenerator *urlGenerator = [[SSVCURLGenerator alloc] initWithBaseURL:baseURL
+                                                                  versionKey:versionKey
+                                                               versionNumber:versionNumber];
+  
+  NSURL *url = [urlGenerator url];
+  NSString *urlString = [url absoluteString];
+  
+  NSString *getKV = [NSString stringWithFormat:@"%@=%@", SSVCLatestVersionNumber, versionNumber];
+  NSRange range = [urlString rangeOfString:getKV options:0];
+  NSUInteger numberOfOccurances = [self numberOfOccurancesOfNeedle:getKV inHaystack:urlString];
+  
+  XCTAssertFalse(range.location == NSNotFound, @"Key-value pair must occur in GET params");
+  XCTAssertEqual(@1, @(numberOfOccurances), @"Only one kv pair should be found together");
+}
+
+- (void)testURLGeneratorContainsVersionValueForVersionKeyWhenBaseURLAlreadyContainsGETParams
 {
   NSString *baseURL = @"http://www.foo.com?foo=bar";
   NSString *versionKey = @"version_key";
@@ -82,13 +130,13 @@
   
   NSString *getKV = [NSString stringWithFormat:@"%@=%@", SSVCLatestVersionKey, versionKey];
   NSRange range = [urlString rangeOfString:getKV options:0];
-  NSUInteger kvLength = [getKV length];
+  NSUInteger numberOfOccurances = [self numberOfOccurancesOfNeedle:getKV inHaystack:urlString];
   
   XCTAssertFalse(range.location == NSNotFound, @"Key-value pair must occur in GET params");
-  XCTAssertEqual(range.length, kvLength, @"Only one kv pair should be found together");
+  XCTAssertEqual(@1, @(numberOfOccurances), @"Only one kv pair should be found together");
 }
 
-- (void)testURLGeneratorContainsVersionNumberValueForVersionNumberKey
+- (void)testURLGeneratorContainsVersionNumberValueForVersionNumberKeyWhenBaseURLAlreadyContainsGETParams
 {
   NSString *baseURL = @"http://www.foo.com?foo=bar";
   NSString *versionKey = @"version_key";
@@ -103,10 +151,92 @@
   
   NSString *getKV = [NSString stringWithFormat:@"%@=%@", SSVCLatestVersionNumber, versionNumber];
   NSRange range = [urlString rangeOfString:getKV options:0];
-  NSUInteger kvLength = [getKV length];
+  NSUInteger numberOfOccurances = [self numberOfOccurancesOfNeedle:getKV inHaystack:urlString];
   
   XCTAssertFalse(range.location == NSNotFound, @"Key-value pair must occur in GET params");
-  XCTAssertEqual(range.length, kvLength, @"Only one kv pair should be found together");
+  XCTAssertEqual(@1, @(numberOfOccurances), @"Only one kv pair should be found together");
+}
+
+- (void)testURLGeneratorContainsLanguageCode
+{
+  NSString *baseURL = @"http://www.foo.com?foo=bar";
+  NSString *versionKey = @"version_key";
+  NSNumber *versionNumber = @1;
+  
+  SSVCURLGenerator *urlGenerator = [[SSVCURLGenerator alloc] initWithBaseURL:baseURL
+                                                                  versionKey:versionKey
+                                                               versionNumber:versionNumber];
+  
+  NSURL *url = [urlGenerator url];
+  NSString *urlString = [url absoluteString];
+
+  NSLocale *locale = [NSLocale currentLocale];
+  NSString *languageCode = [locale objectForKey:NSLocaleLanguageCode];
+  
+  NSString *languageCodeKV = [NSString stringWithFormat:@"%@=%@", SSVCLocaleLanguageCode, languageCode];
+  NSRange range = [urlString rangeOfString:languageCodeKV];
+  
+  NSUInteger numberOfOccurances = [self numberOfOccurancesOfNeedle:languageCodeKV inHaystack:urlString];
+  
+  XCTAssertFalse(range.location == NSNotFound, @"Language code must occure in GET params");
+  XCTAssertEqual(@1, @(numberOfOccurances), @"Language code must only occur once in GET params");
+}
+
+- (void)testURLGeneratorContainsLocaleCode
+{
+  NSString *baseURL = @"http://www.foo.com?foo=bar";
+  NSString *versionKey = @"version_key";
+  NSNumber *versionNumber = @1;
+  
+  SSVCURLGenerator *urlGenerator = [[SSVCURLGenerator alloc] initWithBaseURL:baseURL
+                                                                  versionKey:versionKey
+                                                               versionNumber:versionNumber];
+  
+  NSURL *url = [urlGenerator url];
+  NSString *urlString = [url absoluteString];
+  
+  NSLocale *locale = [NSLocale currentLocale];
+  NSString *countryCode = [locale objectForKey:NSLocaleCountryCode];
+  
+  NSString *countryCodeKV = [NSString stringWithFormat:@"%@=%@", SSVCLocaleCountryCode, countryCode];
+  NSRange range = [urlString rangeOfString:countryCodeKV];
+  
+  NSUInteger numberOfOccurances = [self numberOfOccurancesOfNeedle:countryCode inHaystack:urlString];
+  
+  XCTAssertFalse(range.location == NSNotFound, @"Country code must occure in GET params");
+  XCTAssertEqual(@1, @(numberOfOccurances), @"Country code must only occur once in GET params");
+}
+
+#pragma mark - Utility methods
+
+- (void)testOccuranceOfNeedleMethod
+{
+  NSString *haystack = @"How many pies can a pie eater eat if a pie eater eats all the pie?";
+  
+  NSUInteger flibbleCount = [self numberOfOccurancesOfNeedle:@"flibble" inHaystack:haystack];
+  NSUInteger howCount = [self numberOfOccurancesOfNeedle:@"How" inHaystack:haystack];
+  NSUInteger allCount = [self numberOfOccurancesOfNeedle:@"all" inHaystack:haystack];
+  NSUInteger pieCount = [self numberOfOccurancesOfNeedle:@"pie" inHaystack:haystack];
+  
+  XCTAssertEqual(@0, @(flibbleCount), @"There is no 'flibble'");
+  XCTAssertEqual(@1, @(howCount), @"There is one 'how'");
+  XCTAssertEqual(@1, @(allCount), @"There is one 'all'");
+  XCTAssertEqual(@4, @(pieCount), @"There are lots of pies. 4 to be precise. Pie is tasty!");
+}
+
+- (NSUInteger)numberOfOccurancesOfNeedle:(NSString *)needle inHaystack:(NSString *)haystack
+{
+  NSUInteger count = 0, length = [haystack length];
+  NSRange range = NSMakeRange(0, length);
+  while(range.location != NSNotFound) {
+    range = [haystack rangeOfString:needle options:0 range:range];
+    if(range.location != NSNotFound) {
+      range = NSMakeRange(range.location + range.length, length - (range.location + range.length));
+      count++;
+    }
+  }
+  
+  return count;
 }
 
 
