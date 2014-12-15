38ca6d4d103b92fd2ec0f15ea1bb5aab605d12d6
Tom Elliott <tomelliott@fb.com>
2014-12-15 08:59:02 +0000

    Update test version

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 # and setup the XCTOOL_HOME environment variable to point to the root xctool directory, i.e.
 # `export XCTOOL_HOME='/Users/tomelliott/Code/ObjectiveC/xctool'` in ~/.bashrc or ~/.base_profile
 
-# TODO: Pass in version so we're not stuck to 7.0
-$XCTOOL_HOME/scripts/xctool.sh -project SSVC.xcodeproj -scheme SSVCTests -sdk iphonesimulator7.0 build test
+# TODO: Pass in version so we're not stuck to 8.1
+$XCTOOL_HOME/scripts/xctool.sh -project SSVC.xcodeproj -scheme SSVCTests -sdk iphonesimulator8.1 build test
